592f21b0de8fc0016602424cac86fe1d15e1631d
git2chirag <ramnanichirag70@gmail.com>
2025-12-19 01:54:03 +0530

    optimizng chunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,28 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('reactflow')) {
+              return 'reactflow-vendor';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('draft-js') || id.includes('immutable')) {
+              return 'draft-vendor';
+            }
+            return 'vendor';
+          }
+          if (id.includes('/src/nodes/') || id.includes('/src/components/')) {
+            return 'nodes-components';
+          }
+        }
+      }
+    }
   },
   define: {
     global: 'window', // This aliases 'global' to 'window' for browser builds
